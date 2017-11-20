67e86d65a13b1892a7718329f12572a661ab7972
Ruhee D'cunha <rdcunha2@vt.edu>
2017-11-20 17:21:27 -0500

    pieces in place

--- davidson.py
+++ davidson.py
@@ -4,6 +4,12 @@ import numpy as np
 
 from psi4 import core
 
+def preconditioner(residual, active_mask, A, A_w):
+    precon_resid = np.zeros_like(residual)
+    for x in range(len(active_mask)):
+        precon_resid[:,x] = 1.0/(A[x,x] - A_w[x]) * residual[:,x]
+    return precon_resid
+
 def davidson_solver(mat, ax_function, preconditioner, guess=None, no_eigs=5, maxiter=100):
     """
     Solves for the lowest few eigenvalues and eigenvectors of the given real symmetric matrix
@@ -34,8 +40,9 @@ def davidson_solver(mat, ax_function, preconditioner, guess=None, no_eigs=5, max
 
     """
 
-N = 10
+N = 40
 no_eigs = 5
+nl = 5
 conv = 1.0E-12
 active_mask = [True for x in range(no_eigs)]
 A = np.random.random_integers(-2000,2000,size=(N,N))
@@ -58,12 +65,17 @@ print(B)
 converged=False
 count = 0
 ### begin loop
-while count<5:
+while count<10:
+    # Apply QR decomposition on B to orthogonalize the new vectors wrto all other subspace vectors
+    ## orthogonalize preconditioned residuals against all other vectors in the search subspace
+    B, r = np.linalg.qr(B)
+    print("B")
+    print(B)
     # compute sigma vectors corresponding to the new vectors sigma_i = A B_i
-    sigma = np.zeros((no_eigs, N))
+    sigma = np.zeros((nl, N))
 
     sigma = np.dot(A, B)
-    sigma = sigma[::, :no_eigs]
+    sigma = sigma[::, :nl]
 
     print("sigma")
     print(sigma)
@@ -86,6 +98,9 @@ while count<5:
     print(A_v)
     # here, check if no residuals > max no residuals, if so, collapse subspace
 
+    #if len(B[0] > 20):
+    #    B = np.dot(B, A_v)
+    #    B = B[::, :no_eigs]
     # else, build residual matrix
     ## residual_i = sigma * eigvec - eigval * B * eigvec
     residual = np.zeros((N, no_eigs))
@@ -96,14 +111,30 @@ while count<5:
     print(residual)
     ## check for convergence by norm of residuals
     norm = np.mean(residual, axis=0)**2
+    print("Norm\n", norm)
     if(norm.all() < conv):
         converged = True
         break
-    ## else apply the preconditioner (A_ii - eigval_i)^-1
+    ## else apply the preconditioner (A_ii - A_v_i)^-1
     else:
-        precon_resid = preconditioner(residual, active_mask)
+        precon_resid = preconditioner(residual, active_mask, A, A_w)
+        print("precon_resid matrix")
+        print(precon_resid)
+    ## normalize and add to search subspace if they're larger than a threshold
+        for i in range(no_eigs):
+            to_append = precon_resid[:,i]
+            print("Preconditioned residual ",i, to_append)
+            if np.linalg.norm(to_append) > conv:
+                print("Norm larger than conv. Appending. B: ")
+                B = np.append(B, precon_resid[:,i])
+                nl += 1
+                B = B.reshape(nl,N).T
+                print(B)
+    print("nl")
+    print(nl)
+    print("new B matrix")
+    print(B)
+        
     print(converged)
     count += 1 
-    ## orthogonalize preconditioned residuals against all other vectors in the search subspace
     
-    ## normalize and add to search subspace if they're larger than a threshold
