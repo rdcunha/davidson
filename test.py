7b2754ce3d6c937f025968a1107d1d4d2fff3843
Andrew M. James <amjames2@vt.edu>
2018-01-29 15:14:37 -0500

    Added test-generated output directories to .gitignore, and removed some crazy big random tests that weren't needed

--- test.py
+++ test.py
@@ -23,6 +23,7 @@ parser.add_argument(
 # Controls the interval max for RNG on the diagonal of Random Test matrix
 parser.add_argument('--random_dspread_max', nargs='?', type=float, default=0.5)
 parser.add_argument('--skip_eom', action='store_true', default=False)
+parser.add_argument('--skip_random', action='store_true', default=False)
 parser.add_argument('--out_dir', nargs='?', type=str, default='test_'+datetime.now().isoformat())
 
 h2o_molstr = """
@@ -57,6 +58,8 @@ def main(args):
 
     EOMTestCase.nthread = args.psi4_nthread
     EOMTestCase.memory = '{} GB'.format(args.psi4_mem)
+    smdim = np.random.randint(2000, 3000)
+    lgdim = np.random.randint(6000, 7000)
 
     tests = [
         # each set is [testcasetype (class), args(tuple), kwargs(dict)]
@@ -68,33 +71,30 @@ def main(args):
         [ EOMTestCase, (h2o2_molstr, 'hydrogen-peroxide','cc-pvdz'),dict(no_eigs=2, max_vecs=100, maxiter=300, e_conv=1.0e-6)],
         [ EOMTestCase, (h2o2_molstr, 'hydrogen-peroxide','cc-pvdz'),dict(no_eigs=4, max_vecs=100, maxiter=300, e_conv=1.0e-6)],
         [ EOMTestCase, (h2o2_molstr, 'hydrogen-peroxide','6-31G*'),dict(no_eigs=2, max_vecs=100, maxiter=300, e_conv=1.0e-6)],
-        [ EOMTestCase, (h2o2_molstr, 'hydrogen-peroxide','6-31G'),dict(no_eigs=4, max_vecs=100, maxiter=300, e_conv=1.0e-6)],
+        [ EOMTestCase, (h2o2_molstr, 'hydrogen-peroxide','6-31G'),dict(no_eigs=4, max_vecs=100, maxiter=300, e_conv=1.0e-6, r_conv=1.0e-5)],
         # "smaller" tests 2000-5000 dimension with varying sparsity (1-5 roots)
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 1000), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 100), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 10), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
+        [ NumpyRandomTestCase,(smdim, 1000), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
+        [ NumpyRandomTestCase,(smdim, 100), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
+        [ NumpyRandomTestCase,(smdim, 10), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
         # Stress test, no sparsity factor means non-ideal problem, but given
         # enough iters it *should* work
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 1), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=500, e_conv=1.0e-6) ],
+        [ NumpyRandomTestCase,(smdim, 1), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=3000, e_conv=1.0e-6) ],
         # Stress test, very tight convergence, see how many iters needed
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 1000), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=3000, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 100), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=3000, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(2000, 5000), 10), dict(no_eigs=np.random.randint(3, 5), max_vecs=100, maxiter=3000, e_conv=1.0e-6) ],
-        # "larger" tests 6000-10000 dimension with varying sparsity (1-5 roots)
-        [ NumpyRandomTestCase,(np.random.randint(6000, 10000), 10), dict(no_eigs=np.random.randint(1, 3), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(6000, 10000), 100), dict(no_eigs=np.random.randint(1, 3), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
-        [ NumpyRandomTestCase,(np.random.randint(6000, 10000), 1000), dict(no_eigs=np.random.randint(1, 3), max_vecs=100, maxiter=300, e_conv=1.0e-6) ],
+        [ NumpyRandomTestCase,(smdim, 1000), dict(no_eigs=1, max_vecs=100, maxiter=3000, e_conv=1.0e-10) ],
+        # # Stress test, very larger number of roots
+        [ NumpyRandomTestCase,(smdim, 1000), dict(no_eigs=smdim//100, max_vecs=smdim//10, maxiter=3000, e_conv=1.0e-5, r_conv=1.0e-4) ],
         ]  # yapf: disable
     testing_results = []
     for test_class, case_args, case_kwargs in tests:
         if (test_class == EOMTestCase) and args.skip_eom:
             continue
+        elif (test_class == NumpyRandomTestCase) and args.skip_random:
+            continue
         else:
             case = test_class(*case_args, **case_kwargs)
             case.run()
             testing_results.append(case.checks)
-    with open('allresults.json','w') as f:
-        json.dump(testing_results, f, indent=4)
+
 
 if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
